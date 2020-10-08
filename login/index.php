db2760514998376ba21b0b7ab8426a367a543de3
almerindopaixao <almerindo_paixao@live.com>
2020-10-08 11:38:00 -0300

    update login

---
+++ login/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @link https://github.com/almerindopaixao/pluginswordpress/login
+ * @since 1.0.0
+ * @package Login_Cadastro
+ * Plugin Name: Login e Cadasatro
+ * Description: Plugin de login e cadastro de usuários
+ * version: 1.0.0
+ * Plugin URI: https://github.com/almerindopaixao/pluginswordpress/login 
+ * License: MIT
+ * Author: Almerindo da Paixão Junior
+ * Author URI: https://github.com/almerindopaixao
+*/
